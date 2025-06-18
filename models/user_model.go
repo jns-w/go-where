18a79861c19a385d6aaffdd940518b8ee3a360cf
jns-w <jns.w@icloud.com>
2025-06-18 12:26:30 +0800

    update omit on user model

--- models/user_model.go
+++ models/user_model.go
@@ -1,14 +1,14 @@
 package models
 
 type User struct {
-	ID                        string   `json:"id" bson:"_id,omitempty"`
+	ID                        string   `json:"id,omitempty" bson:"_id,omitempty"`
 	PublicID                  string   `json:"public_id" bson:"public_id"`
 	Username                  string   `json:"username" bson:"username"`
-	Email                     string   `json:"email" bson:"email"`
-	PasswordHash              string   `json:"password_hash" bson:"password_hash"`
-	FavoritePOIs              []string `json:"favorite_pois" bson:"favorite_pois"`
-	LastLocation              GeoPoint `json:"last_location" bson:"last_location"`
-	Friends                   []string `json:"friends" bson:"friends"`
-	PendingFriendRequests     []string `json:"pending_friend_requests" bson:"pending_friend_requests"`
-	PendingFriendRequestsSent []string `json:"pending_friend_requests_sent" bson:"pending_friend_requests_sent"`
+	Email                     string   `json:"email,omitempty" bson:"email,omitempty"`
+	PasswordHash              string   `json:"password_hash,omitempty" bson:"password_hash,omitempty"`
+	FavoritePOIs              []string `json:"favorite_pois,omitempty" bson:"favorite_pois"`
+	LastLocation              GeoPoint `json:"last_location,omitempty" bson:"last_location,omitempty"`
+	Friends                   []string `json:"friends,omitempty" bson:"friends,omitempty"`
+	PendingFriendRequests     []string `json:"pending_friend_requests,omitempty" bson:"pending_friend_requests,omitempty"`
+	PendingFriendRequestsSent []string `json:"pending_friend_requests_sent,omitempty" bson:"pending_friend_requests_sent,omitempty"`
 }
